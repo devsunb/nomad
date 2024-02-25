1ff8d5f494535e054d0d595bd2fe3c2524298f8f
Riccardo Mazzarini <me@noib3.dev>
2024-02-25 22:28:14 +0100

    nomad: implement the `module_name` macro

--- crates/nomad/macros/src/lib.rs
+++ crates/nomad/macros/src/lib.rs
@@ -1,9 +1,31 @@
 use proc_macro::TokenStream;
+use syn::{parse_macro_input, LitStr};
 
 mod module_name;
 
-/// TODO: docs
+/// A proc macro that turns a string literal into a `ModuleName`.
+///
+/// If the string is not a valid module name, the macro will generate a compile
+/// error with a message explaining the problem.
+///
+/// # Examples
+///
+/// ```no_run
+/// # use crate::module_name;
+/// let name: ModuleName = module_name!("foo");
+/// ```
+///
+/// ```compile_fail
+/// # use crate::module_name;
+/// // Fails to compile because the string contains a whitespace.
+/// let name: ModuleName = module_name!("foo bar");
+/// ```
 #[proc_macro]
 pub fn module_name(input: TokenStream) -> TokenStream {
-    module_name::module_name(input)
+    let input = parse_macro_input!(input as LitStr);
+
+    match module_name::module_name(input) {
+        Ok(token_stream) => token_stream.into(),
+        Err(err) => err.to_compile_error().into(),
+    }
 }
--- crates/nomad/src/lib.rs
+++ crates/nomad/src/lib.rs
@@ -1,4 +1,4 @@
-//! # Neovim
+//! # Nomad
 //!
 //! TODO: docs
 
