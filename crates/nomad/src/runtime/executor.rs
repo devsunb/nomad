b91e8a3439adc909901fc8de1f685dd0eeee5b49
Riccardo Mazzarini <me@noib3.dev>
2024-02-29 18:06:52 +0100

    nomad(runtime): add `EXECUTOR`

--- crates/nomad/src/runtime/executor.rs
+++ crates/nomad/src/runtime/executor.rs
@@ -1,14 +1,51 @@
+use core::cell::{OnceCell, UnsafeCell};
 use core::future::Future;
 
 use super::JoinHandle;
 
-struct Executor {}
+thread_local! {
+    static EXECUTOR: OnceCell<UnsafeCell<LocalExecutor>>
+        = const { OnceCell::new() };
+}
+
+/// TODO: docs
+#[derive(Default)]
+struct LocalExecutor {}
+
+impl LocalExecutor {
+    /// TODO: docs
+    #[inline]
+    fn spawn<F: Future>(&mut self, future: F) -> JoinHandle<F::Output>
+    where
+        F: Future + 'static,
+        F::Output: 'static,
+    {
+        todo!();
+    }
+}
+
+/// TODO: docs
+#[inline]
+fn with_executor<F, R>(fun: F) -> R
+where
+    F: FnOnce(&mut LocalExecutor) -> R,
+{
+    let executor_ptr = EXECUTOR
+        .with(|executor| executor.get_or_init(UnsafeCell::default).get());
+
+    // SAFETY: we never give out references to the executor, but can we prove
+    // that the function is not reentrant?
+    let executor = unsafe { &mut *executor_ptr };
+
+    fun(executor)
+}
 
 /// TODO: doc
+#[inline]
 pub fn spawn<F>(future: F) -> JoinHandle<F::Output>
 where
     F: Future + 'static,
     F::Output: 'static,
 {
-    todo!();
+    with_executor(move |executor| executor.spawn(future))
 }
