238c79d016ef502602a5e83084c5b8d9ed29a55a
Riccardo Mazzarini <me@noib3.dev>
2024-05-24 16:35:34 +0800

    nvimx: make it a workspace

---
+++ lib/nvimx/nvimx-executor/src/lib.rs
@@ -0,0 +1,8 @@
+//! TODO: docs
+
+#![cfg_attr(docsrs, feature(doc_cfg))]
+
+extern crate alloc;
+
+mod executor;
+mod task;
--- lib/nvimx/src/lib.rs
+++ lib/nvimx/src/lib.rs
@@ -2,12 +2,8 @@
 
 #![cfg_attr(docsrs, feature(doc_cfg))]
 
-extern crate alloc;
-
 pub use nvim_oxi as oxi;
 
 #[cfg(feature = "executor")]
 #[cfg_attr(docsrs, doc(cfg(feature = "executor")))]
 pub mod executor;
-#[cfg(feature = "executor")]
-mod task;
