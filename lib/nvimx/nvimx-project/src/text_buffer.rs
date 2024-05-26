3d5e375bc76c8fd0976d0ae60760e9d9ff4b8ae4
Riccardo Mazzarini <me@noib3.dev>
2024-05-26 16:11:18 +0800

    nvimx(project): implement `TextBuffer::current()`

--- lib/nvimx/nvimx-project/src/text_buffer.rs
+++ lib/nvimx/nvimx-project/src/text_buffer.rs
@@ -1,25 +1,52 @@
+use api::opts::OptionOpts;
+use nvim_oxi::api::{self, Buffer};
 use nvimx_common::Apply;
 
 /// TODO: docs.
-pub struct TextBuffer {}
+pub struct TextBuffer {
+    _inner: Buffer,
+}
 
 impl TextBuffer {
     /// TODO: docs.
     #[inline]
     pub fn current() -> Result<Self, NotTextBufferError> {
-        todo!();
+        let buffer = Buffer::current();
+
+        let buftype: String = {
+            let opts = OptionOpts::builder().buffer(buffer.clone()).build();
+            api::get_option_value("buftype", &opts).expect("always set")
+        };
+
+        match buftype.as_ref() {
+            "" => Ok(Self { _inner: buffer }),
+            "help" => Err(NotTextBufferError::Help),
+            "quickfix" => Err(NotTextBufferError::Quickfix),
+            "terminal" => Err(NotTextBufferError::Terminal),
+            _ => panic!("unknown buftype: {}", buftype),
+        }
     }
 
     /// TODO: docs.
     #[inline]
-    pub fn edit<E>(&mut self, _edit: E) -> <Self as Apply<E>>::Diff
+    pub fn edit<E>(&mut self, edit: E) -> <Self as Apply<E>>::Diff
     where
         Self: Apply<E>,
     {
-        todo!();
+        self.apply(edit)
     }
 }
 
-/// TODO: docs.
+/// Error type returned by [`TextBuffer::current`] when the current buffer
+/// is not a text buffer.
 #[derive(Debug)]
-pub enum NotTextBufferError {}
+pub enum NotTextBufferError {
+    /// The current buffer is a help file.
+    Help,
+
+    /// The current buffer is a quickfix list.
+    Quickfix,
+
+    /// The current buffer house a terminal emulator.
+    Terminal,
+}
