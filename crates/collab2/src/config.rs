b3accbb86fe19563630ce60a6b3c6bac164f9792
Riccardo Mazzarini <me@noib3.dev>
2024-09-17 16:58:05 +0200

    collab2: add `Config::nomad_dir()`

--- crates/collab2/src/config.rs
+++ crates/collab2/src/config.rs
@@ -1,3 +1,11 @@
-#[derive(Debug, Default, serde::Deserialize)]
+use collab_fs::AbsUtf8PathBuf;
+
+#[derive(Debug, Default, Clone, serde::Deserialize)]
 #[serde(deny_unknown_fields)]
 pub struct Config {}
+
+impl Config {
+    pub(crate) fn nomad_dir(&self) -> AbsUtf8PathBuf {
+        todo!();
+    }
+}
