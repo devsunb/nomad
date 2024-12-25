2935b17e749c4aedf726af01f6492885f76a94c1
Riccardo Mazzarini <me@noib3.dev>
2024-12-25 19:15:44 +0800

    nvimx: add `Backend::{Api,to_backend_api()}`

--- lib/nvimx2/backend-neovim/src/neovim.rs
+++ lib/nvimx2/backend-neovim/src/neovim.rs
@@ -1,8 +1,13 @@
 use core::marker::PhantomData;
 
-use nvimx_core::Backend;
+use nvimx_core::{Backend, Plugin, PluginApi};
 
-use crate::{NeovimBackgroundExecutor, NeovimLocalExecutor, NeovimVersion};
+use crate::{
+    oxi,
+    NeovimBackgroundExecutor,
+    NeovimLocalExecutor,
+    NeovimVersion,
+};
 
 /// TODO: docs.
 pub struct Neovim<V: NeovimVersion> {
@@ -10,11 +15,23 @@ pub struct Neovim<V: NeovimVersion> {
 }
 
 impl<V: NeovimVersion> Backend for Neovim<V> {
+    type Api<P: Plugin<Self>> = oxi::Dictionary;
     type LocalExecutor = NeovimLocalExecutor;
     type BackgroundExecutor = NeovimBackgroundExecutor;
 
     #[inline]
     fn init() -> Self {
+        todo!();
+    }
+
+    #[inline]
+    fn to_backend_api<P>(
+        &mut self,
+        _plugin_api: PluginApi<P, Self>,
+    ) -> Self::Api<P>
+    where
+        P: Plugin<Self>,
+    {
         todo!();
     }
 }
--- lib/nvimx2/nvimx-core/src/backend.rs
+++ lib/nvimx2/nvimx-core/src/backend.rs
@@ -1,7 +1,11 @@
 use crate::executor::{BackgroundExecutor, LocalExecutor};
+use crate::{Plugin, PluginApi};
 
 /// TODO: docs.
-pub trait Backend: 'static {
+pub trait Backend: 'static + Sized {
+    /// TODO: docs.
+    type Api<P: Plugin<Self>>;
+
     /// TODO: docs.
     type LocalExecutor: LocalExecutor;
 
@@ -10,4 +14,12 @@ pub trait Backend: 'static {
 
     /// TODO: docs.
     fn init() -> Self;
+
+    /// TODO: docs.
+    fn to_backend_api<P>(
+        &mut self,
+        plugin_api: PluginApi<P, Self>,
+    ) -> Self::Api<P>
+    where
+        P: Plugin<Self>;
 }
