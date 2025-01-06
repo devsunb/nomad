a428af915d327badb4ef50736fbc82318ceb1797
Riccardo Mazzarini <me@noib3.dev>
2025-01-06 18:14:30 +0800

    Over-engineer the shit out of build script

--- crates/version/build.rs
+++ crates/version/build.rs
@@ -1,38 +1,57 @@
 #![allow(missing_docs)]
+#![allow(clippy::unwrap_used)]
 
-use std::borrow::Cow;
 use std::env;
+use std::fmt::Write;
 use std::fs::File;
-use std::io::Write;
 use std::path::Path;
 
 fn main() {
-    let commit_hash = "c26db43";
-    let commit_year = "2025";
-    let commit_month = "1";
-    let commit_day = "17";
-    let major = "0";
-    let minor = "1";
-    let patch = "0";
-    let pre_release_label = Some("dev");
+    let mut file = GeneratedFile::default();
+    add_commit_infos(&mut file);
+    add_version_infos(&mut file);
+    file.create();
+}
 
-    write!(
-        out_file("generated.rs"),
-        r#"
-pub(crate) const COMMIT_SHORT_HASH: &'static str = "{commit_hash}";
-pub(crate) const COMMIT_YEAR: u16 = {commit_year};
-pub(crate) const COMMIT_MONTH: u8 = {commit_month};
-pub(crate) const COMMIT_DAY: u8 = {commit_day};
-pub(crate) const MAJOR: u8 = {major};
-pub(crate) const MINOR: u8 = {minor};
-pub(crate) const PATCH: u8 = {patch};
-pub(crate) const PRE_RELEASE_LABEL: Option<&'static str> = {pre};
-        "#,
-        pre = pre_release_label
-            .map(|pre| Cow::Owned(format!("Some(\"{pre}\")")))
-            .unwrap_or(Cow::Borrowed("None")),
-    )
-    .expect("couldn't write to file");
+fn add_commit_infos(file: &mut GeneratedFile) {
+    file.add_const("COMMIT_SHORT_HASH", "c26db43")
+        .add_const("COMMIT_YEAR", 2025u16)
+        .add_const("COMMIT_MONTH", 1u8)
+        .add_const("COMMIT_DAY", 17u8);
+}
+
+fn add_version_infos(file: &mut GeneratedFile) {
+    file.add_const("MAJOR", 0u8)
+        .add_const("MINOR", 1u8)
+        .add_const("PATCH", 0u8)
+        .add_const("PRE_RELEASE_LABEL", Some("dev"));
+}
+
+#[derive(Default)]
+struct GeneratedFile {
+    contents: String,
+}
+
+impl GeneratedFile {
+    const NAME: &'static str = "generated.rs";
+
+    fn add_const<T: DisplayType>(
+        &mut self,
+        name: &str,
+        value: T,
+    ) -> &mut Self {
+        write!(&mut self.contents, "pub(crate) const {name}: ").unwrap();
+        T::display_type(&mut self.contents);
+        self.contents.push_str(" = ");
+        T::display_value(&value, &mut self.contents);
+        self.contents.push_str(";\n");
+        self
+    }
+
+    fn create(self) {
+        use std::io::Write;
+        write!(out_file(Self::NAME), "{}", self.contents).unwrap()
+    }
 }
 
 /// Opens the file with the given name in the `OUT_DIR`, or creates a new one
@@ -43,4 +62,60 @@ fn out_file(file_name: &str) -> File {
     File::create(&out_path).unwrap_or_else(|err| {
         panic!("couldn't create file at {out_path:?}: {err}")
     })
+}
+
+trait DisplayType {
+    fn display_type(buf: &mut String);
+    fn display_value(&self, buf: &mut String);
+}
+
+impl DisplayType for u8 {
+    fn display_type(buf: &mut String) {
+        buf.push_str("u8");
+    }
+
+    fn display_value(&self, buf: &mut String) {
+        write!(buf, "{self}").unwrap()
+    }
+}
+
+impl DisplayType for u16 {
+    fn display_type(buf: &mut String) {
+        buf.push_str("u16");
+    }
+
+    fn display_value(&self, buf: &mut String) {
+        write!(buf, "{self}").unwrap()
+    }
+}
+
+impl DisplayType for &str {
+    fn display_type(buf: &mut String) {
+        buf.push_str("&str");
+    }
+
+    fn display_value(&self, buf: &mut String) {
+        write!(buf, "\"{self}\"").unwrap()
+    }
+}
+
+impl<T: DisplayType> DisplayType for Option<T> {
+    fn display_type(buf: &mut String) {
+        buf.push_str("Option<");
+        T::display_type(buf);
+        buf.push('>');
+    }
+
+    fn display_value(&self, buf: &mut String) {
+        match self {
+            Some(value) => {
+                write!(buf, "Some(").unwrap();
+                value.display_value(buf);
+                write!(buf, ")").unwrap();
+            },
+            None => {
+                buf.push_str("None");
+            },
+        }
+    }
 }
