93339b2940d4f80ea75945be727456c10c92f106
Riccardo Mazzarini <me@noib3.dev>
2025-01-25 18:46:06 +0800

    Rename `ServerSocket` to `ServerAddress`

--- crates/collab2/src/config/mod.rs
+++ crates/collab2/src/config/mod.rs
@@ -1,11 +1,12 @@
-mod server_socket;
+mod server_address;
 
-pub(crate) use server_socket::ServerSocket;
+pub(crate) use server_address::ServerAddress;
 
 #[derive(Debug, Default, serde::Deserialize)]
 #[serde(default)]
 #[serde(deny_unknown_fields)]
 pub struct Config {
-    /// TODO: docs.
-    pub(crate) server_socket: ServerSocket,
+    /// The address of the server to connect to when starting or joining an
+    /// editing session.
+    pub(crate) server_address: ServerAddress,
 }
--- crates/collab2/src/config/server_address.rs
+++ crates/collab2/src/config/server_address.rs
@@ -5,23 +5,23 @@ use std::rc::Rc;
 use serde::de::{Deserialize, Deserializer};
 
 #[derive(Clone)]
-pub(crate) struct ServerSocket {
+pub(crate) struct ServerAddress {
     inner: Rc<str>,
 }
 
-impl Default for ServerSocket {
+impl Default for ServerAddress {
     fn default() -> Self {
         Self { inner: "collab.nomad.foo:64420".to_owned().into() }
     }
 }
 
-impl fmt::Debug for ServerSocket {
+impl fmt::Debug for ServerAddress {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_tuple("ServerSocket").field(&self.inner).finish()
+        f.debug_tuple("ServerAddress").field(&self.inner).finish()
     }
 }
 
-impl Deref for ServerSocket {
+impl Deref for ServerAddress {
     type Target = str;
 
     fn deref(&self) -> &Self::Target {
@@ -29,7 +29,7 @@ impl Deref for ServerSocket {
     }
 }
 
-impl<'de> Deserialize<'de> for ServerSocket {
+impl<'de> Deserialize<'de> for ServerAddress {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
     where
         D: Deserializer<'de>,
