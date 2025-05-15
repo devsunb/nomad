02c9d889986c90b55cf62fe259bbfd64531b7d46
Riccardo Mazzarini <me@noib3.dev>
2025-05-16 01:21:37 +0200

    feat(tests): implement `ed::on_cursor_created()`

--- tests/src/ed/cursor.rs
+++ tests/src/ed/cursor.rs
@@ -1,4 +1,24 @@
-use ed::EditorCtx;
 use ed::backend::Backend;
+use ed::{EditorCtx, Shared};
+use futures_lite::future;
 
-pub(crate) fn on_cursor_created<Ed: Backend>(_ctx: &mut EditorCtx<'_, Ed>) {}
+pub(crate) fn on_cursor_created<Ed: Backend>(ctx: &mut EditorCtx<'_, Ed>) {
+    let agent_id = ctx.new_agent_id();
+
+    let num_called = Shared::new(0);
+
+    let _handle = ctx.on_cursor_created({
+        let num_called = num_called.clone();
+        move |_cursor, created_by| {
+            assert_eq!(created_by, agent_id);
+            num_called.with_mut(|count| *count += 1);
+        }
+    });
+
+    future::block_on(ctx.spawn_local(async |ctx| {
+        // Focusing the buffer should create a cursor.
+        ctx.create_and_focus(path!("/foo.txt"), agent_id).await.unwrap();
+    }));
+
+    assert_eq!(num_called.copied(), 1);
+}
