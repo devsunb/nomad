0736378621921ebff0028ee466ad024dc37d5c05
Riccardo Mazzarini <me@noib3.dev>
2025-08-17 22:53:25 +0200

    feat(tests): make sure tasks still resolve even if `ThreadPool` is dropped

--- tests/src/thread_pool/mod.rs
+++ tests/src/thread_pool/mod.rs
@@ -11,6 +11,15 @@ fn task_resolves_to_future_output() {
 }
 
 #[test]
+fn task_resolves_if_thread_pool_is_dropped() {
+    let value = 42;
+    let mut thread_pool = ThreadPool::default();
+    let task = thread_pool.spawn(async move { value });
+    drop(thread_pool);
+    assert_eq!(future::block_on(task), value);
+}
+
+#[test]
 #[should_panic]
 fn panic_is_propagated_if_task_is_awaited() {
     let mut thread_pool = ThreadPool::default();
