d41454e6f588691bbe0c905db7ae7fd51d116fcb
Riccardo Mazzarini <me@noib3.dev>
2025-09-05 13:11:05 +0200

    feat(auth): sketch sending a login request and opening the user's browser

--- crates/auth/src/config.rs
+++ crates/auth/src/config.rs
@@ -1,5 +1,3 @@
-use crate::github;
-
 /// The configuration of the [`Auth`](crate::Auth) module.
 #[derive(Debug, Default, serde::Deserialize)]
 #[serde(default)]
@@ -7,5 +5,5 @@ use crate::github;
 pub struct Config {
     #[cfg(feature = "neovim")]
     #[serde(flatten)]
-    pub(crate) github: github::GitHubConfig,
+    pub(crate) github: crate::github::GitHubConfig,
 }
--- crates/auth/src/github.rs
+++ crates/auth/src/github.rs
@@ -1,36 +1,92 @@
 //! TODO: docs.
 
-use auth_types::{AuthInfos, OAuthState};
+use std::io;
+use std::sync::LazyLock;
+
+use auth_types::{AuthInfos, GitHubAccessToken, OAuthState};
 use editor::{Access, Context, Editor};
 use rand::Rng;
 use url::Url;
+
+static GITHUB_AUTHORIZE_URL: LazyLock<Url> = LazyLock::new(|| {
+    Url::parse("https://github.com/login/oauth/authorize").expect("valid URL")
+});
 
 #[derive(Debug, serde::Deserialize)]
 #[serde(default)]
 #[serde(deny_unknown_fields)]
 pub(crate) struct GitHubConfig {
-    /// The base URL to send the login request to.
-    pub login_request_url: Url,
+    /// The URL where the authentication server is running.
+    auth_server_url: Url,
 }
 
 pub(crate) async fn login<Ed: Editor>(
-    _config: impl Access<GitHubConfig>,
+    config: impl Access<GitHubConfig>,
     ctx: &mut Context<Ed>,
 ) -> Result<AuthInfos, GitHubLoginError> {
-    let _state = OAuthState::from_bytes(ctx.with_rng(Rng::random));
+    let auth_server_url = config.with(|config| config.auth_server_url.clone());
+
+    let oauth_state = OAuthState::from_bytes(ctx.with_rng(Rng::random));
+
+    let login_request = ctx.spawn_background({
+        let auth_server_url = auth_server_url.clone();
+        async move { login_request(&auth_server_url, &oauth_state).await }
+    });
+
+    let open_browser = ctx.spawn_background(async move {
+        open_browser(&auth_server_url, &oauth_state)
+    });
 
     todo!();
 }
 
+async fn login_request(
+    auth_server_url: &Url,
+    oauth_state: &OAuthState,
+) -> reqwest::Result<GitHubAccessToken> {
+    let login_url = auth_server_url
+        .join(&format!("/github/login/{oauth_state}"))
+        .expect("route is valid");
+
+    reqwest::get(login_url).await?.json::<GitHubAccessToken>().await
+}
+
+fn open_browser(
+    auth_server_url: &Url,
+    oauth_state: &OAuthState,
+) -> io::Result<()> {
+    let callback_url =
+        auth_server_url.join("/github/callback").expect("route is valid");
+
+    let mut github_authorize_url = (&*GITHUB_AUTHORIZE_URL).clone();
+
+    github_authorize_url
+        .query_pairs_mut()
+        .append_pair("client_id", auth_types::NOMAD_GITHUB_CLIENT_ID.as_str())
+        .append_pair("scope", "read:user user:email")
+        .append_pair("state", &oauth_state.to_string())
+        .append_pair("redirect_uri", callback_url.as_str());
+
+    open::that(github_authorize_url.as_str())
+}
+
 /// TODO: docs.
-#[derive(Debug, PartialEq, Eq, derive_more::Display)]
+#[derive(Debug, derive_more::Display)]
 #[display("{_0}")]
-pub enum GitHubLoginError {}
+pub enum GitHubLoginError {
+    /// The login request to the authentication server failed.
+    #[display("Login request to the authentication server failed: {_0}")]
+    LoginRequest(reqwest::Error),
+
+    /// The user's web browser couldn't be opened.
+    #[display("Couldn't open URL in web browser: {_0}")]
+    OpenBrowser(io::Error),
+}
 
 impl Default for GitHubConfig {
     fn default() -> Self {
         Self {
-            login_request_url: Url::parse("https://auth.collab.foo")
+            auth_server_url: Url::parse("https://auth.collab.foo")
                 .expect("valid URL"),
         }
     }
